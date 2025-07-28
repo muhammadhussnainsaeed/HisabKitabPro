496680a79a1d9d3c440d4138c2669b05a70f07f2
muhammadhussnainsaeed <m.hussnainsaeed@gmail.com>
2025-07-28 17:45:51 +0500

    Initial commit

---
+++ HisabKitabPro/HisabKitabProApp.swift
@@ -0,0 +1,17 @@
+//
+//  HisabKitabProApp.swift
+//  HisabKitabPro
+//
+//  Created by Hussnain on 28/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HisabKitabProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HisabKitabProTests/HisabKitabProTests.swift
@@ -0,0 +1,16 @@
+//
+//  HisabKitabProTests.swift
+//  HisabKitabProTests
+//
+//  Created by Hussnain on 28/7/25.
+//
+
+import Testing
+
+struct HisabKitabProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
